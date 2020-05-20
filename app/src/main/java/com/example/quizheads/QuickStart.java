14e1438d7bb4254fcf5b62f13c7f2d9b93167f71
Adela Turcovska <>
2020-05-20 17:03:36 +0200

    popup

--- app/src/main/java/com/example/quizheads/QuickStart.java
+++ app/src/main/java/com/example/quizheads/QuickStart.java
@@ -1,72 +1,120 @@
 package com.example.quizheads;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
-import static java.lang.Math.random;
 
 public class QuickStart extends AppCompatActivity  {
-        String questionNumber = "Question number";
-        int number = 1;
-        StartLearning mapa = new StartLearning();
+    int number = 1;
+    int correctAnswerCounter = 0;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quickstart);
+        Button finalRes = findViewById(R.id.popUpButton);
 
-        @Override
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_quickstart);
-
-            TextView textViewQuestion = findViewById(R.id.questionNameTextView);
-            EditText textForAnswer = findViewById(R.id.answerEditText);
-            Button showResult = findViewById(R.id.showResultButton);
-            RecyclerView recyclerView = findViewById(R.id.recycler_view_learning_countries);
-            showQuestion();
-            increaseQuestionNumber();
-        }
-
-        public void increaseQuestionNumber() {
-            final TextView textViewQuestionNumber = findViewById(R.id.questionNumber);
-            TextView textViewQuestion = findViewById(R.id.questionNameTextView);
-            Button nextQuestion = findViewById(R.id.nextQuestionButton);
-
-            textViewQuestionNumber.setText("Question number " + number);
-            nextQuestion.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (number <= 9) {
-                        number++;
-                        textViewQuestionNumber.setText("Question number " + number);
-                        showQuestion();
-                    }
-                }});
+        finalRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(QuickStart.this, PopUpResults.class);
+                startActivity(i);
             }
+        });
+        showQuestion();
+        increaseQuestionNumber(correctAnswerCounter);
+    }
 
-        @SuppressLint("SetTextI18n")
-        public void showQuestion() {
-            TextView textViewQuestion = findViewById(R.id.questionNameTextView);
+    @SuppressLint("SetTextI18n")
+    public void increaseQuestionNumber(int correctAnswerCounter) {
+        Button nextQuestion = findViewById(R.id.nextQuestionButton);
+        final TextView textViewQuestionNumber = findViewById(R.id.questionNumber);
+        final TextView textViewQuestion = findViewById(R.id.questionNameTextView);
+        final Button showResult = findViewById(R.id.showResultButton);
+        final EditText textForAnswer = findViewById(R.id.answerEditText);
+        showResult.setBackgroundResource(android.R.drawable.btn_default);
+        nextQuestion.setBackgroundResource(android.R.drawable.btn_default);
+        textViewQuestionNumber.setText(getString(R.string.question_number_2) + number);
+        nextQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (number <= 9) {
+                    number++;
+                    textViewQuestionNumber.setText(getString(R.string.question_number_2) + number);
+                    showResult.setBackgroundResource(android.R.drawable.btn_default);
+                    textForAnswer.setText("");
+                    showResult.setEnabled(true);
+                    showQuestion();
+                }
+                if (number == 10) {
+                    textViewQuestionNumber.setText("You finished the quick test");
+                    textViewQuestion.setText("Let's see how you did!");
+                    textForAnswer.setVisibility(View.INVISIBLE);
+                    finalResults();
+                }
+            }
+        });
+    }
 
-            ArrayList<String> keyList = new ArrayList<String>(mapa.getMapCountries().keySet());
-            ArrayList<String> valueList = new ArrayList<String>(mapa.getMapCountries().values());
+    @SuppressLint("SetTextI18n")
+    public int showQuestion() {
+        final TextView textViewQuestion = findViewById(R.id.questionNameTextView);
+        final Button showResult = findViewById(R.id.showResultButton);
+        final EditText textForAnswer = findViewById(R.id.answerEditText);
+        Map<String, String> getMapOfCountries = DataHelper.createMapOfCountries(this);
+        final ArrayList<String> keyList = new ArrayList<String>(getMapOfCountries.keySet());
+        final ArrayList<String> valueList = new ArrayList<String>(getMapOfCountries.values());
 
-            int randomIndex1 = (int) (Math.random() * 10);
+        final int indexOfQuestionNumbers = (int) (Math.random() * 10);
+        final ArrayList<String> listOfQuestions = new ArrayList<>();
 
-            ArrayList<String> list1 = new ArrayList<>();
-            list1.add("What is the capital city of: " + keyList.get(randomIndex1));
-            list1.add("In which state is this city located : " + valueList.get(randomIndex1));
+        listOfQuestions.add(getString(R.string.question_capital_city) + keyList.get(indexOfQuestionNumbers));
+        listOfQuestions.add(getString(R.string.question_country) + valueList.get(indexOfQuestionNumbers));
 
-            int randomIndex2 = (int) (Math.random() * list1.size());
-            textViewQuestion.setText(list1.get(randomIndex2));
-        }
+        final int randomIndex2 = (int) (Math.random() * listOfQuestions.size());
+        final String random1 = listOfQuestions.get(randomIndex2);
+        textViewQuestion.setText(random1);
+
+        showResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textFromAnswer = textForAnswer.getText().toString().toLowerCase();
+                if(randomIndex2 == 1 && textFromAnswer.contains(keyList.get(indexOfQuestionNumbers).toLowerCase())) {
+                    showResult.setBackgroundColor(Color.parseColor("#00BB11"));
+                    showResult.setEnabled(false);
+                    correctAnswerCounter++;
+                } else if(randomIndex2 == 0 && textFromAnswer.equals(valueList.get(indexOfQuestionNumbers).toLowerCase())){
+                    showResult.setBackgroundColor(Color.parseColor("#00BB11"));
+                    showResult.setEnabled(false);
+                    correctAnswerCounter++;
+                } else {
+                    showResult.setBackgroundColor(Color.parseColor("#FF0000"));
+                }
+            }
+        });
+        return correctAnswerCounter;
+    }
+
+    public void finalResults() {
+        Button nextQuestion = findViewById(R.id.nextQuestionButton);
+        Button showResult = findViewById(R.id.showResultButton);
+        Button finalRes = findViewById(R.id.popUpButton);
+        TextView resultText = findViewById(R.id.textView2);
+
+        finalRes.setVisibility(View.VISIBLE);
+        nextQuestion.setVisibility(View.INVISIBLE);
+        showResult.setVisibility(View.INVISIBLE);
+    }
 }
+
