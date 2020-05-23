08b73337e42742ffc5e9605daeec05a46d70648a
Vojtech Jancik <jancik.vojtech@gmail.com>
2020-05-23 17:51:47 +0200

    Add all countries and its capital citites.

--- app/src/main/java/com/example/quizheads/PopUpResults.java
+++ app/src/main/java/com/example/quizheads/PopUpResults.java
@@ -8,21 +8,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PopUpResults extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        final TextView textViewScore = findViewById(R.id.textView);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popup);
 
         Button closePopUpButton = findViewById(R.id.closePopUpButton);
+
         closePopUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(PopUpResults.this, MainActivity.class);
-                startActivity(i);
+                Intent intent = new Intent(PopUpResults.this, MainActivity.class);
+                Bundle bundle = intent.getExtras();
+
+                startActivity(intent);
             }
         });
 
@@ -33,14 +39,10 @@ public class PopUpResults extends AppCompatActivity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int) (width * .8), (int) (height * .6));
-
-        NumberOfRightAnswers();
     }
 
     private void NumberOfRightAnswers() { //TODO
-        final TextView resultText = findViewById(R.id.textView);
-
-        //resultText.setText("correctAnswers");
+        TextView resultText = findViewById(R.id.textView);
 
     }
 }
